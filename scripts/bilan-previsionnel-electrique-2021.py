16e1a89f9de053582a2d6921a9ba05aba4adf556
Océan Sicard--Baux <ocean.sicard-baux@etu.umontpellier.fr>
2022-12-02 09:20:45 +0100

    bilan previsionnel : ajout total

--- scripts/bilan-previsionnel-electrique-2021.py
+++ scripts/bilan-previsionnel-electrique-2021.py
@@ -4,20 +4,10 @@ bilanprevisionnel = pandas.read_csv(
     "rawdata/bilan-previsionnel-electrique-2021-offre-de-production.csv", sep=";"
 )
 
-# on conserve que les attributs utiles
-bilanprevisionnel = bilanprevisionnel[
-    [
-        "annee",
-        "nucleaire_gw",
-        "cycles_combines_au_gaz_gw",
-        "turbines_a_combustion_gw",
-        "hydraulique_gw",
-        "eolien_gw",
-        "photovoltaique_gw",
-    ]
-]
 
-# pas besoin de renommer, c'est clair dès quand on sait que gw = gigawatt
+total = bilanprevisionnel.sum(axis=1)
+bilanprevisionnel["total"] = total
+
 bilanprevisionnel.to_csv(
     "cleaneddata/bilan-previsionel-electrique-2021-offre-de-production.csv", index=False
 )
