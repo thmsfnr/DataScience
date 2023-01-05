fdfd287578494a06fbaeec85fc398d996579405b
b0pol <bopol@e.email>
2023-01-05 15:32:47 +0100

    données consommation électricité 2001-2021

---
+++ prevision-consommation/prevision-consommation.py
@@ -0,0 +1,14 @@
+# https://www.edf.fr/groupe-edf/espaces-dedies/l-energie-de-a-a-z/tout-sur-l-energie/l-electricite-au-quotidien/la-consommation-d-electricite-en-chiffres
+
+# https://www.rte-france.com/actualites/bilan-electrique-francais-2019-une-consommation-en-baisse-depuis-10-ans-une-production
+
+
+# rte -> soit essayer de récupérer via l'API, soit extraire manuellement les données des PDFs
+
+# https://bilan-electrique-2020.rte-france.com/#
+# https://bilan-electrique-2021.rte-france.com/#
+
+# https://bilan-electrique-2021.rte-france.com/consommation_evolution_de_la_consommation-2
+# https://odre.opendatasoft.com/explore/dataset/consommation-annuelle-brute/table/?sort=-annee
+# différence consommation brute / corrigée expliquée ici https://www.edf.fr/groupe-edf/espaces-dedies/l-energie-de-a-a-z/tout-sur-l-energie/l-electricite-au-quotidien/la-consommation-d-electricite-en-chiffres
+
