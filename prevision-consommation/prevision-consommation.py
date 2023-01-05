334dc495e1598d603048123979766b2d6567a53b
b0pol <bopol@e.email>
2023-01-05 15:49:34 +0100

    ajout précision consommation corrigée

--- prevision-consommation/prevision-consommation.py
+++ prevision-consommation/prevision-consommation.py
@@ -1,7 +1,5 @@
 # https://www.edf.fr/groupe-edf/espaces-dedies/l-energie-de-a-a-z/tout-sur-l-energie/l-electricite-au-quotidien/la-consommation-d-electricite-en-chiffres
-
 # https://www.rte-france.com/actualites/bilan-electrique-francais-2019-une-consommation-en-baisse-depuis-10-ans-une-production
-
 
 # rte -> soit essayer de récupérer via l'API, soit extraire manuellement les données des PDFs
 
@@ -12,3 +10,18 @@
 # https://odre.opendatasoft.com/explore/dataset/consommation-annuelle-brute/table/?sort=-annee
 # différence consommation brute / corrigée expliquée ici https://www.edf.fr/groupe-edf/espaces-dedies/l-energie-de-a-a-z/tout-sur-l-energie/l-electricite-au-quotidien/la-consommation-d-electricite-en-chiffres
 
+import pandas
+
+conso = pandas.read_csv("cleaneddata/consommation-anuelle-electricite-2001-2021.csv")
+print(conso)
+
+
+# brute : c'est la consommation totale d'électricité. 
+# Elle comprend non seulement la consommation finale des consommateurs 
+# mais également la consommation nécessaire à fournir l'électricité,
+# les pertes dûes à la transformation et au transport et les écarts statistiques telles que la météorologie.
+
+# corrigée : c'est la consommation corrigée des aléas climatiques 
+# (les températures sont remplacées par des températures de référence) 
+# et des effets calendaires (comme les années bissextiles).
+# Elle permet d’observer plus finement les évolutions structurelles.
