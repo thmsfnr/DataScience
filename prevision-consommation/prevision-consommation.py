7f87cab99d546c61d93325939b632be07b5e1e58
Océan Sicard--Baux <ocean.sicard-baux@etu.umontpellier.fr>
2023-01-05 16:26:40 +0100

    prevision consommation

--- prevision-consommation/prevision-consommation.py
+++ prevision-consommation/prevision-consommation.py
@@ -12,6 +12,7 @@
 
 import pandas
 import matplotlib.pyplot as plt
+from prophet import Prophet
 
 df = pandas.read_csv("cleaneddata/consommation-anuelle-electricite-2001-2021.csv")
 
@@ -35,6 +36,7 @@ df["conso_corrigee"] = df["conso_corrigee"] * 1000
 df["annee"] = pandas.to_datetime(df["annee"], format="%Y")
 
 df.columns = ["ds", "y"]
+print(df)
 
 ### graphique
 plt.figure(figsize=(17, 8))
@@ -46,4 +48,20 @@ plt.grid(False)
 # plt.show()
 plt.savefig("prevision-consommation/consommation-2001-2021-france.png")
 
-print(df)
+
+### prédiction
+
+# Creation objet prophet
+m = Prophet()
+# Entrainement du modèle
+m.fit(df)
+# prediction des 10 prochaines années par défault ça compare avec les données historiques
+future = m.make_future_dataframe(periods=10, freq="Y")
+forecast = m.predict(future)
+fig = m.plot(forecast)
+plt.xlabel("Année")
+plt.ylabel("Consommation d'électricité (en MWh)")
+plt.title("Consommation d'électricité (en MWh) en France, en fonction des années")
+# plt.show()
+plt.savefig("prevision-consommation/prediction-consommation.png")
+
