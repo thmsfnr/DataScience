47781bf2118166bc340b935e6d18ac708f1a3ffb
Thomas Fournier <thomas42600.f@gmail.com>
2023-01-14 17:21:10 +0100

    update

--- dashboard/app.py
+++ dashboard/app.py
@@ -16,7 +16,6 @@ import frame5
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.H1("Analyse de l'arret de l'utilisation du nucléaire dans la production d'électricité en France"),
-    # add a design dropdown
     dcc.Dropdown(
         id='page-selector',
         options=[
